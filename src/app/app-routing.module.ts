47b400562da0f5cbe9819cb64a6de221f8807dc1
khan162 <umarahmedkhank6@gmail.com>
2023-06-21 23:23:34 -0400

    changed routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SchedulePageComponent } from './pages/schedule-page/schedule-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomePageComponent },
+  { path: 'home', component: SchedulePageComponent },
   { path: 'about', component: SchedulePageComponent },
-];  
+];
 //add route here after creating component
 
 @NgModule({
